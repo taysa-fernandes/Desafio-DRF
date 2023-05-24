dda81c9fcfc5ef69b987db2f8b4b7971a37ead27
taysa-fernandes <taysamirelly34@gmail.com>
2023-05-24 20:24:06 -0300

    programa testado e adaptado

--- core/urls.py
+++ core/urls.py
@@ -30,7 +30,5 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ] + router.urls
--- core/views.py
+++ core/views.py
@@ -24,46 +24,38 @@ class AlunoviewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     
     @swagger_auto_schema(
-        method = 'post',
         request_body = None,
         operation_description = "Método de criar aluno",
         operation_summary="Cria um novo aluno",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['POST'])
     def create(self, request):
         '''Criando um Aluno'''
         return  super().create(request)
     @swagger_auto_schema(
-        method = 'put',
         request_body = None,
-        operation_description = "Método de atualizar os dados de um aluno",
-        operation_summary="Atualiza os dados de um aluno",
+        operation_description = "Método de atualizar os dados de um aluno inserindo todos os dados",
+        operation_summary="Atualiza os dados de um aluno inserindo todos os dados",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PUT'])
     def update(self, request, *args, **kwargs):
         '''Atualizando os dados de um aluno'''
         return super().update(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'delete',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID do Aluno", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de deletar os dados de um aluno",
         operation_summary="Deleta os dados de um aluno",
         responses = {200: openapi.Response('Sucess')}
     )
-    @api_view(['DELETE'])
     def destroy(self, request, *args, **kwargs):
         '''Excluindo os dados de um aluno'''
         return super().destroy(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'get',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID do Aluno", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de listar os dados de um aluno específico",
         operation_summary="Lista os dados de um aluno específico",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['GET'])
     def retrieve(self, request, *args, **kwargs):
         '''Listando dados de um aluno específico'''
         return super().retrieve(request, *args, **kwargs)
@@ -83,15 +75,13 @@ class AlunoviewSet(viewsets.ModelViewSet):
        return super().list(request,*args,**kwargs)
        
     @swagger_auto_schema(
-        method = 'patch',
         request_body = AlunoSerializer,
-        operation_description = "Método de atualizar os dados de um aluno inserindo todos os dados",
-        operation_summary="Atualiza os dados de um aluno inserindo todos os dados",
+        operation_description = "Método de atualizar os dados de um aluno",
+        operation_summary="Atualiza os dados de um aluno",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PATCH'])
     def partial_update(self, request, *args, **kwargs):
-        '''Atualizando parcialmente os dados de um aluno'''
+        '''Atualizando os dados de um aluno'''
         return super().partial_update(request, *args, **kwargs)
     
 class ProfessorViewSet(viewsets.ModelViewSet):
@@ -102,51 +92,43 @@ class ProfessorViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     
     @swagger_auto_schema(
-        method = 'post',
         request_body = None,
         operation_description = "Método de criar professores",
         operation_summary="Cria um novo professor",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['POST'])
     def create(self, request):
         '''Criando um Professor'''
         return super().create(request)
     @swagger_auto_schema(
-        method = 'put',
         request_body = None,
         properties={
             'Nome': openapi.Schema(type=openapi.TYPE_STRING),
             'matricula': openapi.Schema(type=openapi.TYPE_INTEGER),
             'disciplinas': openapi.Schema(type=openapi.TYPE_INTEGER),
         },
-        operation_description = "Método de atualizar os dados de um professor",
-        operation_summary="Atualiza os dados de um professor",
+        operation_description = "Método de atualizar os dados de um professor inserindo todos os dados",
+        operation_summary="Atualiza os dados de um professor inserindo todos os dados",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PUT'])
     def update(self, request, *args, **kwargs):
         '''Atualizando os dados de um professor'''
         return super().update(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'delete',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID do Professor", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de deletar os dados de um professor",
         operation_summary="Deleta os dados de um professor",
         responses = {200: openapi.Response('Sucess')}
     )
-    @api_view(['DELETE'])
     def destroy(self, request, *args, **kwargs):
         '''Excluindo os dados de um professor'''
         return super().destroy(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'get',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID do Professor", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de listar os dados de um Professor específico",
         operation_summary="Lista os dados de um professor específico",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['GET'])
     def retrieve(self, request, *args, **kwargs):
         '''Listando dados de um professor específico'''
         return super().retrieve(request, *args, **kwargs)
@@ -165,13 +147,11 @@ class ProfessorViewSet(viewsets.ModelViewSet):
         '''Listando os professores cadastrados'''
         return super().list(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'patch',
         request_body = ProfessorSerializer,
-        operation_description = "Método de atualizar os dados de um professor inserindo todos os dados",
-        operation_summary="Atualiza os dados de um professor inserindo todos os dados",
+        operation_description = "Método de atualizar os dados de um professor",
+        operation_summary="Atualiza os dados de um professor",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PATCH'])
     def partial_update(self, request, *args, **kwargs):
         '''Atualizando parcialmente os dados de um professor'''
         return super().partial_update(request, *args, **kwargs)
@@ -186,46 +166,38 @@ class DisciplinaViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     
     @swagger_auto_schema(
-        method = 'post',
         request_body = None,
         operation_description = "Método de criar disciplina",
         operation_summary="Cria uma nova disciplina",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['POST'])
     def create(self, request):
         '''Criando uma Disciplina'''
         return super().create(request)
     @swagger_auto_schema(
-        method = 'put',
         request_body = None,
-        operation_description = "Método de atualizar os dados de uma disciplina",
-        operation_summary="Atualiza os dados de uma disciplina",
+        operation_description = "Método de atualizar os dados de uma disciplina inserindo todos os dados",
+        operation_summary="Atualiza os dados de uma disciplina inserindo todos os dados",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PUT'])
     def update(self, request, *args, **kwargs):
         '''Atualizando Disciplina'''
         return super().update(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'delete',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID da Disciplina", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de deletar os dados de uma disciplina",
         operation_summary="Deleta os dados de uma disciplina",
         responses = {200: openapi.Response('Sucess')}
     )
-    @api_view(['DELETE'])
     def destroy(self, request, *args, **kwargs):
         '''Excluindo os dados de uma disciplina'''
         return super().destroy(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'get',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID da Disciplina", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de listar os dados de uma disciplina específica",
         operation_summary="Lista os dados de uma disciplina específica",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['GET'])
     def retrieve(self, request, *args, **kwargs):
         '''Listando dados de uma disciplina específica'''
         return super().retrieve(request, *args, **kwargs)
@@ -246,15 +218,13 @@ class DisciplinaViewSet(viewsets.ModelViewSet):
         '''Listando as disciplinas cadastrados'''
         return super().list(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'patch',
         request_body = DisciplinaSerializer,
-        operation_description = "Método de atualizar os dados de uma disciplina inserindo todos os dados",
-        operation_summary="Atualiza os dados de uma disciplina inserindo todos os dados",
+        operation_description = "Método de atualizar os dados de uma disciplina",
+        operation_summary="Atualiza os dados de uma disciplina",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PATCH'])
     def partial_update(self, request, *args, **kwargs):
-        '''Atualizando parcialmente os dados de uma disciplina'''
+        '''Atualizando os dados de uma disciplina'''
         return super().partial_update(request, *args, **kwargs)
     
 
@@ -267,46 +237,38 @@ class CursoViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     
     @swagger_auto_schema(
-        method = 'post',
         request_body = None,
         operation_description = "Método de criar Curso",
         operation_summary="Cria um novo curso",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['POST'])
     def create(self, request):
         '''Criando um Curso'''
         return super().create(request)
     @swagger_auto_schema(
-        method = 'put',
         request_body = None,
         operation_description = "Método de atualizar os dados de um curso",
         operation_summary="Atualiza os dados de um curso",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PUT'])
     def update(self, request, *args, **kwargs):
         '''Atualizando os dados de um curso'''
         return super().update(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'delete',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID do Curso", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de deletar os dados de um curso",
         operation_summary="Deleta os dados de um curso",
         responses = {200: openapi.Response('Sucess')}
     )
-    @api_view(['DELETE'])
     def destroy(self, request, *args, **kwargs):
         '''Excluindo os dados de um curso'''
         return super().destroy(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'get',
         manual_parameters=[openapi.Parameter('id', openapi.IN_PATH, description="ID do Curso", type=openapi.TYPE_INTEGER)],
         operation_description = "Método de listar os dados de um curso específico",
         operation_summary="Lista os dados de um curso específico",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['GET'])
     def retrieve(self, request, *args, **kwargs):
         '''Listando dados de um curso específico'''
         return super().retrieve(request, *args, **kwargs)
@@ -324,13 +286,11 @@ class CursoViewSet(viewsets.ModelViewSet):
         '''Listando os cursos cadastrados'''
         return super().list(request, *args, **kwargs)
     @swagger_auto_schema(
-        method = 'patch',
         request_body = CursoSerializer,
         operation_description = "Método de atualizar os dados de um curso inserindo todos os dados",
         operation_summary="Atualiza os dados de um curso inserindo todos os dados",
         responses = {200: openapi.Response('Sucess'),400: openapi.Response('Bad Request'),401: openapi.Response('Unauthorized'),500: openapi.Response('Server error')}
     )
-    @api_view(['PATCH'])
     def partial_update(self, request, *args, **kwargs):
         '''Atualizando parcialmente os dados de um curso'''
         return super().partial_update(request, *args, **kwargs)
